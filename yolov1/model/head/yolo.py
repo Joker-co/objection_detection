a3c12712aadf2a8603ef5bd9e86695b7e76ecbfb
Joker-co <hujh960215@163.com>
2021-11-18 15:11:35 +0800

    feat(head): Add yolov1 head

---
+++ yolov1/model/head/yolo.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class Yolov1Head(nn.Module):
+    def __init__(self, inplanes, num_classes):
+        super(Yolov1Head, self).__init__()
+        self.num_classes = num_classes
+        # objectness, classes, locations
+        self.out_channels = 1 + self.num_classes + 4
+        self.pred = nn.Conv2d(inplanes, self.out_channels, kernel_size=1)
+
+    def forward(self. x):
+        feat = x['feats']
+
+        out = self.pred(feat)
+        x.update({'feats': out})
+        return x
